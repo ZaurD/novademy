a3ac4f4bdb73e05e7b818184b38b8fa34366e27d
narminasuleymanova <118384923+narminasuleymanova@users.noreply.github.com>
2023-06-22 18:10:03 +0400

    added new block

---
+++ src/app/components/landing/why-novademy/why-novademy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-why-novademy',
+  templateUrl: './why-novademy.component.html',
+  styleUrls: ['./why-novademy.component.scss']
+})
+export class WhyNovademyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
